1d38bfaece4cdc6a4dc5e7cd6488da5a5d382570
Alejandro González <ale2793@hotmail.com>
2019-02-24 18:01:04 -0600

    Added Serialization header file

---
+++ include/oxylus/configuration/Serialization.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C) 2018
+ * SIPLab Tecnológico de Costa Rica
+ *
+ * Project: Oxylus Framework
+ *
+ * Check the LICENCE.txt file for further information
+ *
+ * \author: Pablo Alvarado
+ * \date:   2018-08-31
+ */
+
+#ifndef OXYLUS_SERIALIZATION_H
+#define OXYLUS_SERIALIZATION_H
+
+#include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/vector.hpp>
+
+
+#endif
