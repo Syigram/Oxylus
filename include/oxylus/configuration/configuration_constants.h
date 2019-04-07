dc880f99694a8995c774c851f2d898ab629e67c0
Alejandro Gonzalez <ale2793@hotmail.com>
2019-04-07 16:56:22 -0600

    added configuration constants file

---
+++ include/oxylus/configuration/configuration_constants.h
@@ -0,0 +1,25 @@
+
+#ifndef CONFIGURATION_PARAMETERS_H
+#define CONFIGURATION_PARAMETERS_H
+
+
+
+namespace rdf  {
+  namespace bpc  {
+    const char * CONFIGFILE_PATH = "/home/agonzalez/tfg/oxylus_github/build/config.ini";
+
+    const int IMAGE_AVG_SIZE = 100;
+
+
+    const int BLACK = 0;
+    const int BLUE = 1;
+    const int GREEN = 2;
+    const int CIAN = 3;
+    const int RED = 4;
+    const int MAGENTA = 5;
+    const int YELLOW = 6;
+    const int WHITE = 7;
+  }
+}
+
+#endif
