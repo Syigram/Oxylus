7a7f756e0d536dd5cc2a6ab4ea38176309df69bb
Alejandro Gonzalez <ale2793@hotmail.com>
2019-04-14 15:04:27 -0600

    cell templated implementation

--- include/oxylus/training/cell.h
+++ include/oxylus/training/cell.h
@@ -1,18 +1,28 @@
 #ifndef CELL_H
 #define CELL_H
 
+/* #include <oxylus */
+
 namespace rdf{
   namespace bpc{
 
+    template <class T>
     class Cell{
       public:
         Cell();
         Cell(const Cell& orig);
-        std::vector<float> left;      ///< Left histogram
-        std::vector<float> right;     ///< Right histogram
+        void AddToHistogram(bool left, T value );
+        void AddToLeftHistogram(T value);
+        void AddToRightHistogram(T value);
         virtual ~Cell();
+      private:
+        std::vector<float> leftHistogram;      ///< Left histogram
+        std::vector<float> rightHistogram;     ///< Right histogram
     };
+
   }
 }
+
+#include "cell.tpp"
 
 #endif /* CELL_H */
