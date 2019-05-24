1a6eb587501116e7c97498e08025c39bb761c9c3
Alejandro Gonzalez <ale2793@hotmail.com>
2019-05-24 13:43:31 -0600

    added Node base class files

--- include/oxylus/training/node.h
+++ include/oxylus/training/node.h
@@ -1,10 +1,33 @@
+/*
+ * File:   Node.h
+ * Author: Sygram
+ *
+ * Class: rdf::Node
+ * Needs revision: Maybe. Consult with coordinator.
+ *                 More abstract methods could be declared.
+ * Description: Pure abstract class for all nodes entities.
+ *
+ */
+
 #ifndef NODE_H
 #define NODE_H
 
-class Node {
+
+namespace rdf {
+
+  class Node {
   public:
     Node();
-    ~Node();
-};
+    Node(const Node& orig);
+    virtual ~Node();
+    virtual Node * GetLeft() = 0;
+    virtual Node * GetRight() = 0;
+  private:
+
+  };
+
+} /* rdf */
+
+
 
 #endif /* NODE_H */
---
+++ src/training/node.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   Node.cpp
+ * Author: Sygram
+ *
+ * Created on May 14, 2018, 2:07 AM
+ */
+
+#include "Node.h"
+
+using namespace rdf;
+
+Node::Node() {
+}
+
+Node::Node(const Node& orig) {
+}
+
+Node::~Node() {
+}
