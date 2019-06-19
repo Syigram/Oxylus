08189c2668b9c1c95115a0578eee1f14129f1092
Alejandro Gonzalez <ale2793@hotmail.com>
2019-06-19 10:34:50 -0600

    added support for printing nodeVectors's vectors

--- include/oxylus/training/node_vectors.h
+++ include/oxylus/training/node_vectors.h
@@ -3,9 +3,11 @@
 
 #include <vector>
 
+#include <oxylus/configuration/Serialization.h>
+#include <oxylus/training/matrix.h>
+#include <oxylus/training/cell.h>
 #include <oxylus/training/features.h>
 
-#include <oxylus/configuration/Serialization.h>
 
 namespace rdf {
   namespace bpc {
@@ -19,6 +21,7 @@ namespace rdf {
           ar & thresholdsVec;
           ar & featuresVec;
           ar & nodeId;
+          ar & nodeHistograms;
         }
 
       public:
@@ -28,10 +31,56 @@ namespace rdf {
           this->thresholdsVec = thresholdsVec;
           this->featuresVec = featuresVec;
         }
+        NodeVectors(const NodeVectors& other) {
+          this->nodeId = other.nodeId;
+          this->featuresVec = other.featuresVec;
+          this->thresholdsVec = other.thresholdsVec;
+          this->nodeHistograms = other.nodeHistograms;
+        }
+        NodeVectors& operator=(const NodeVectors& rhs) {
+          this->nodeId = rhs.nodeId;
+          this->featuresVec = rhs.featuresVec;
+          this->thresholdsVec = rhs.thresholdsVec;
+          this->nodeHistograms = rhs.nodeHistograms;
+          return *this;
+        }
+
+        void PrintLeftHistograms() {
+          for (int i = 0; i < featuresVec->size(); ++i) {
+            for (int j = 0; j < thresholdsVec->size(); ++j) {
+              std::cout << "nodeVector[0]:" <<  nodeHistograms[i][j].leftHistogram[0] << std::endl;        
+              std::cout << "nodeVector[1]:" <<  nodeHistograms[i][j].leftHistogram[1] << std::endl;        
+              std::cout << "nodeVector[2]:" <<  nodeHistograms[i][j].leftHistogram[2] << std::endl;        
+              std::cout << "nodeVector[3]:" <<  nodeHistograms[i][j].leftHistogram[3] << std::endl;        
+              std::cout << "nodeVector[4]:" <<  nodeHistograms[i][j].leftHistogram[4] << std::endl;        
+              std::cout << "nodeVector[5]:" <<  nodeHistograms[i][j].leftHistogram[5] << std::endl;        
+              std::cout << "nodeVector[6]:" <<  nodeHistograms[i][j].leftHistogram[6] << std::endl;        
+            }
+          }
+        }
+
+        void PrintRightHistograms() {
+          for (int i = 0; i < featuresVec->size(); ++i) {
+            for (int j = 0; j < thresholdsVec->size(); ++j) {
+              std::cout << "nodeVector[0]:" <<  nodeHistograms[i][j].rightHistogram[0] << std::endl;        
+              std::cout << "nodeVector[1]:" <<  nodeHistograms[i][j].rightHistogram[1] << std::endl;        
+              std::cout << "nodeVector[2]:" <<  nodeHistograms[i][j].rightHistogram[2] << std::endl;        
+              std::cout << "nodeVector[3]:" <<  nodeHistograms[i][j].rightHistogram[3] << std::endl;        
+              std::cout << "nodeVector[4]:" <<  nodeHistograms[i][j].rightHistogram[4] << std::endl;        
+              std::cout << "nodeVector[5]:" <<  nodeHistograms[i][j].rightHistogram[5] << std::endl;        
+              std::cout << "nodeVector[6]:" <<  nodeHistograms[i][j].rightHistogram[6] << std::endl;        
+            }
+          }
+        }
+
         int nodeId;
         std::vector<int>* thresholdsVec;
         std::vector<Features>* featuresVec;
-        virtual ~NodeVectors(){};
+        Matrix<Cell> nodeHistograms;
+        virtual ~NodeVectors(){
+          /* delete thresholdsVec; */
+          /* delete featuresVec; */
+        };
     };
   }
 }
